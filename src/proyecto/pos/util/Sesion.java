dca5f25df2d4e848d531f25393a8b9d30ac436c7
jsacramentoc2-jpg <jsacramentoc2@upao.edu.pe>
2026-07-08 21:06:43 -0500

    Update Sesion.java

--- src/proyecto/pos/util/Sesion.java
+++ src/proyecto/pos/util/Sesion.java
@@ -1,1 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto.pos.util;
 
+/**
+ *
+ * @author LENOVO
+ */
+
+
+public class Sesion {
+
+    private static final int EMPLEADO_ID_POR_DEFECTO = 174;
+
+    private static Integer empleadoActualId = null;
+
+    private Sesion() {
+    }
+
+    public static void setEmpleadoActualId(int id) {
+        empleadoActualId = id;
+    }
+
+    public static int getEmpleadoActualId() {
+        return empleadoActualId != null ? empleadoActualId : EMPLEADO_ID_POR_DEFECTO;
+    }
+
+    public static void cerrarSesion() {
+        empleadoActualId = null;
+    }
+}
